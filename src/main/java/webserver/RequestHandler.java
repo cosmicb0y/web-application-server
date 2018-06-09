8b71714e0cf75dfe73f1b01ec47c19e1cb0f0802
cosmicb0y <jang.hw73@gmail.com>
2018-06-09 23:07:14 +0900

    Implemented HTTP GET signup

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,7 +3,9 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
@@ -16,6 +18,8 @@ public class RequestHandler extends Thread {
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
     }
+
+    public User user;
 
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
@@ -31,13 +35,27 @@ public class RequestHandler extends Thread {
                 line = br.readLine();
             }
 
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            httpResponse(out, url);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void httpResponse(OutputStream out, String url) throws IOException {
+        DataOutputStream dos = new DataOutputStream(out);
+        File urlFile = new File("./webapp" + url);
+
+        if (url.contains("/user/create")) {
+            int index = url.indexOf("?");
+            String param = url.substring(index+1);
+            Map<String, String> paramMap = HttpRequestUtils.parseQueryString(param);
+            user = new User(paramMap.get("userId"), paramMap.get("password"), paramMap.get("name"), paramMap.get("email"));
+            urlFile = new File("./webapp" + "/index.html");
+        }
+
+        byte[] body = Files.readAllBytes(urlFile.toPath());
+        response200Header(dos, body.length);
+        responseBody(dos, body);
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
