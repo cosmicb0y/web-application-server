37d99bc91e4804dd23b2488f4182ba7a8b3ce62d
cosmicb0y <jang.hw73@gmail.com>
2018-06-10 16:18:42 +0900

    Implemented signIn

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -29,8 +29,13 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
+            if (db == null) {
+                db = new DataBase();
+            }
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             String line = br.readLine();
+            String action = line.split(" ")[0];
             String url = HttpRequestUtils.parseURL(line);
             int length = 0;
             while (!"".equals(line)) {
@@ -38,6 +43,7 @@ public class RequestHandler extends Thread {
                 if (line.contains("Content-Length")) {
                     length = Integer.parseInt(line.split(" ")[1]);
                 }
+                log.debug(line);
                 line = br.readLine();
             }
 
@@ -45,10 +51,19 @@ public class RequestHandler extends Thread {
             File urlFile = new File("./webapp" + url);
 
             if (url.contains("/user/create")) {
+                signUp(br, length, dos);
+                return;
+            }
+
+            if (action.equals("POST") && url.contains("/user/login")) {
                 String param = IOUtils.readData(br, length);
                 Map<String, String> paramMap = HttpRequestUtils.parseQueryString(param);
-                db.addUser(new User(paramMap.get("userId"), paramMap.get("password"), paramMap.get("name"), paramMap.get("email")));
-                response302Header(dos,  "/index.html");
+                User signInUser = db.findUserById(paramMap.get("userId"));
+                if (signInUser != null && signInUser.getPassword().equals(paramMap.get("password"))) {
+                    responseSignInHeader(dos, "logined=true", "/index.html");
+                    return;
+                }
+                responseSignInHeader(dos, "logined=false", "/user/login_failed.html");
                 return;
             }
 
@@ -59,6 +74,13 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void signUp(BufferedReader br, int length, DataOutputStream dos) throws IOException {
+        String param = IOUtils.readData(br, length);
+        Map<String, String> paramMap = HttpRequestUtils.parseQueryString(param);
+        db.addUser(new User(paramMap.get("userId"), paramMap.get("password"), paramMap.get("name"), paramMap.get("email")));
+        response302Header(dos,  "/index.html");
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
@@ -72,10 +94,22 @@ public class RequestHandler extends Thread {
         }
     }
 
+    private void responseSignInHeader(DataOutputStream dos, String cookie, String redirectURL) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Set-Cookie: " + cookie + "\r\n");
+            dos.writeBytes("Location: " + redirectURL + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
     private void response302Header(DataOutputStream dos, String redirectURL) {
         try {
             dos.writeBytes("HTTP/1.1 302 Found\r\n");
             dos.writeBytes("Location: "+ redirectURL + "\r\n");
+            dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
         }
