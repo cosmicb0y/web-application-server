03eba86e203f16ec134072d760bea02b572a3e82
cosmicb0y <jang.hw73@gmail.com>
2018-06-10 15:23:02 +0900

    Implemented 302 status code

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,6 +5,7 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.util.Map;
 
+import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,7 +21,7 @@ public class RequestHandler extends Thread {
         this.connection = connectionSocket;
     }
 
-    public User user;
+    public DataBase db;
 
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
@@ -46,8 +47,9 @@ public class RequestHandler extends Thread {
             if (url.contains("/user/create")) {
                 String param = IOUtils.readData(br, length);
                 Map<String, String> paramMap = HttpRequestUtils.parseQueryString(param);
-                user = new User(paramMap.get("userId"), paramMap.get("password"), paramMap.get("name"), paramMap.get("email"));
-                urlFile = new File("./webapp" + "/index.html");
+                db.addUser(new User(paramMap.get("userId"), paramMap.get("password"), paramMap.get("name"), paramMap.get("email")));
+                response302Header(dos,  "/index.html");
+                return;
             }
 
             byte[] body = Files.readAllBytes(urlFile.toPath());
@@ -65,6 +67,15 @@ public class RequestHandler extends Thread {
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, String redirectURL) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found\r\n");
+            dos.writeBytes("Location: "+ redirectURL + "\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
         }
